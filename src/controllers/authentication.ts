93f7d79f2b0abcc5911a7d73e1bc58cdb5c0e989
nuhptr <nugrahaadi733@gmail.com>
2023-08-31 17:38:58 +0700

    add login endpoint

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { getUserByEmail, createUser } from '../db/users';
 import { authentication, random } from '../helpers';
 
+//? register endpoint
 export const register = async (
   req: express.Request,
   res: express.Response
@@ -44,3 +45,60 @@ export const register = async (
   }
 };
 
+//? Login endpoint
+export const login = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400).json({
+        message: 'Email/Password Not Valid!',
+      });
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+
+    if (!user) {
+      return res.sendStatus(400).json({
+        message: 'User Not Found!',
+      });
+    }
+
+    const expectedHash = authentication(
+      user.authentication?.salt,
+      password
+    );
+
+    if (user.authentication?.password !== expectedHash) {
+      return res.sendStatus(400).json({
+        message: 'Wrong Password!',
+      });
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie('ADI-AUTH', user.authentication?.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+
+    // return json
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res
+      .sendStatus(400)
+      .json({ message: 'Authentication Failed' });
+  }
+};
