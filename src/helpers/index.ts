e5255ed163949d9139d76914980b83040c0e0b91
nuhptr <nugrahaadi733@gmail.com>
2023-08-31 12:43:21 +0700

    helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto';
+
+const SECRET = 'ADI-REST-API';
+
+export const random = () => {
+  return crypto.randomBytes(128).toString('base64');
+};
+
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac('sha256', [salt, password].join('/'))
+    .update(SECRET)
+    .digest('hex');
+};
